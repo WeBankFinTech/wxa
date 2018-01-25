8c36dbc9d285789d2e0e59d31e65cc0e437a2c17
iveswen <iveswen@webank.com>
2018-01-26 00:06:49 +0800

    inital commit

---
+++ packages/wxa-plugin-replace/index.js
@@ -0,0 +1,22 @@
+module.exports = class ReplacePlugin {
+    constructor(options={}) {
+        this.configs = Object.assign({}, options);
+    }   
+    apply(compiler) {
+        compiler.hooks.optimizeAssets.tapAsync('optimize-assets', (code, compilation, next)=>{
+            this.run(code, compilation, next);
+        })
+    }
+    run(code, compilation, next) {
+        if(this.configs.list.length) {
+            this.configs.list.forEach((rep)=>{
+                code = code.replace(rep.regular, rep.value);
+            });
+            
+            compilation.code = code;
+            next();
+        } else {
+            next();
+        }
+    }
+}
