ef03e8206a64890e606ab9dd19afa832421ff5d8
Genuifx <1119127128@qq.com>
2018-01-29 21:36:47 +0800

    fix: use compile-config for json compile

--- packages/wxa-cli/src/compile-config.js
+++ packages/wxa-cli/src/compile-config.js
@@ -1,4 +1,4 @@
-import {getDistPath, writeFile} from './utils';
+import {getDistPath, writeFile, readFile} from './utils';
 import {log} from 'util';
 import path from 'path';
 import {AsyncSeriesHook} from 'tapable';
@@ -9,11 +9,16 @@ class CConfig {
         this.src = src || 'src';
         this.dist = dist || 'dist';
         this.type = 'json';
+        this.code = '';
         this.hooks = {
             optimizeAssets: new AsyncSeriesHook(['code', 'compilation']),
         };
     }
     compile(content, opath) {
+        if (content == null) {
+            content = readFile(path.join(opath.dir, opath.base));
+            if (content == null) throw new Error('打开文件失败 ', path.join(opath.dir, opath.base));
+        }
         this.code = content;
         return this.hooks.optimizeAssets.promise(content, this).then((err)=>{
             if (err) return Promise.reject(err);
