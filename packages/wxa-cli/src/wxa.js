35bc08c86d5a00118a2228cf757fc105612a6937
Genuifx <1119127128@qq.com>
2018-01-30 20:48:00 +0800

    feat: read babelrc or pkg.babel configurations

--- packages/wxa-cli/src/wxa.js
+++ packages/wxa-cli/src/wxa.js
@@ -4,13 +4,16 @@ import chalk from 'chalk';
 import {info} from './utils';
 
 const version = require('../package.json').version;
-info('Hello', `This is ${chalk.keyword('orange')('wxa@'+version)}, Running in ${chalk.keyword('orange')(process.env.NODE_ENV || 'development')}`);
 
 commander.usage('[command] <options ...>');
 commander.option('-w, --watch', '监听文件改动');
 commander.command('build').description('编译项目').action((projectpath)=>{
     // console.log(commander);
+    info('Hello', `This is ${chalk.keyword('orange')('wxa@'+version)}, Running in ${chalk.keyword('orange')(process.env.NODE_ENV || 'development')}`);
     compiler.build(commander);
+});
+commander.command('version').description('版本').action((projectpath)=>{
+    info('version', version);
 });
 
 commander.parse(process.argv);
