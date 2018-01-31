e28546e6e0304b354464abf7e49c2d84464f0f32
Genuifx <1119127128@qq.com>
2018-01-31 15:19:45 +0800

    feat: 支持注入到component

--- packages/wxa-redux/src/index.js
+++ packages/wxa-redux/src/index.js
@@ -1,23 +1,34 @@
 import mapState from './mapState'
 import {createStore, applyMiddleware} from 'redux'
 
+let mountRedux = function(originMount){
+    return function(...args) {
+        this.unsubscribe = this.store.subscribe((...args)=>{
+            let state = this.store.getState();
+            let data = mapState(this.mapState, state);
+            
+            if(this.$isCurrentPage && data != null) this.setData(data);
+        });
+        originMount.apply(this, args);
+    }
+}
+
+let unmountRedux = function(originUnmount) {
+    return function(...args) {
+        if(this.unsubscribe) this.unsubscribe();
+        originUnmount.apply(this, args);
+    }
+}
+
 export const wxaRedux = ({reducers, middlewares}, type)=>{
     return (vm)=>{
         if (type === 'App') {
             vm.store = createStore(reducers, applyMiddleware(...middlewares));
-        } else {
+        } else if(type === 'Page'){
             if (vm.app == null) throw new Error('wxa-redux需要使用@GetApp修饰符');
             vm.store = vm.app.store;
             let {onLoad, onShow, onUnload, onHide} = vm;
-            vm.onLoad = function(...args) {
-                this.unsubscribe = this.store.subscribe((...args)=>{
-                    let state = this.store.getState();
-                    let data = mapState(this.mapState, state);
-                    
-                    if(this.$isCurrentPage && data != null) this.setData(data);
-                });
-                onLoad.apply(this, args);
-            }
+            vm.onLoad = mountRedux(onLoad);
             vm.onShow = function(...args) {
                 this.$isCurrentPage = true;
                 let data = mapState(this.mapState, state);
@@ -28,10 +39,11 @@ export const wxaRedux = ({reducers, middlewares}, type)=>{
                 this.$isCurrentPage = false;
                 onHide.apply(this, args);
             }
-            vm.onUnload = function(...args) {
-                if(this.unsubscribe) this.unsubscribe();
-                onUnload.apply(this, args);
-            }
+            vm.onUnload = unmountRedux(onUnload);
+        } else if(type === 'Component') {
+            let {created, attached, detached} = vm;
+            vm.attached = mountRedux(attached);
+            vm.detached = unmountRedux(detached);
         }
     };
 }
