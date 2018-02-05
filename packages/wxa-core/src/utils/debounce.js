14cd75467c3b95c5abd204b8c17fee5d722b43b0
Genuifx <1119127128@qq.com>
2018-02-06 00:05:41 +0800

    chroes: add git hook

--- packages/wxa-core/src/utils/debounce.js
+++ packages/wxa-core/src/utils/debounce.js
@@ -1,7 +1,7 @@
 /**
  *
- * @param fn {Function}   实际要执行的函数
- * @param delay {Number}  延迟时间，也就是阈值，单位是毫秒（ms）
+ * @param {Function} fn   实际要执行的函数
+ * @param {Number} delay  延迟时间，也就是阈值，单位是毫秒（ms）
  *
  * @return {Function}     返回一个“去弹跳”了的函数
  */
