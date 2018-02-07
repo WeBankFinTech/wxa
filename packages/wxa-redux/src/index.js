2250ac1f6209401558ef74c5c6eb2d7dcb4ddfcf
iveswen <iveswen@webank.com>
2018-02-08 00:53:35 +0800

    chores: fix some staff, and write test suites

--- packages/wxa-redux/src/index.js
+++ packages/wxa-redux/src/index.js
@@ -1,61 +1,84 @@
 import mapState from './mapState'
-import {createStore, applyMiddleware} from 'redux'
+import {
+    createStore,
+    applyMiddleware
+} from 'redux'
 
-let mountRedux = function(originMount){
-    return function(...args) {
-        if(this.store) {
-            this.unsubscribe = this.store.subscribe((...args)=>{
+let mountRedux = function (originMount) {
+    return function (...args) {
+        this.unsubscribe = this.store.subscribe((...args) => {
+            if(this.$isCurrentPage) {
                 let newState = this.store.getState();
                 let data = mapState(this.mapState, newState, this.data);
-                
-                if(this.$isCurrentPage && data != null) this.setData(data);
-            });
+
+                if (data !== null) {
+                    this.setData(data);
+                }
+            }
+        });
+        if (originMount) originMount.apply(this, args);
+    }
+}
+
+let unmountRedux = function (originUnmount) {
+    return function (...args) {
+        if (this.unsubscribe) {
+            this.unsubscribe();
+            this.unsubscribe = null;
         }
-        if(originMount) originMount.apply(this, args);
+        if (originUnmount) originUnmount.apply(this, args);
     }
 }
 
-let unmountRedux = function(originUnmount) {
-    return function(...args) {
-        if(this.unsubscribe) this.unsubscribe();
-        if(originUnmount) originUnmount.apply(this, args);
-    }
-}
-
-export const wxaRedux = ({reducers, middlewares}, type)=>{
-    return (vm)=>{
+export const wxaRedux = ({
+    reducers,
+    middlewares
+}, type) => {
+    return (vm) => {
         if (type === 'App') {
             let args = [reducers];
-            if(middlewares) args.push(applyMiddleware(...middlewares));
+            if (middlewares) args.push(applyMiddleware(...middlewares));
             vm.store = createStore.apply(null, args);
-        } else if(type === 'Page'){
+        } else if (type === 'Page') {
             if (vm.app == null) throw new Error('wxa-redux需要使用@GetApp修饰符');
             vm.store = vm.app.store;
-            let {onLoad, onShow, onUnload, onHide} = vm;
+            let {
+                onLoad,
+                onShow,
+                onUnload,
+                onHide
+            } = vm;
             vm.onLoad = mountRedux(onLoad);
-            vm.onShow = function(...args) {
+            vm.onShow = function (...args) {
                 this.$isCurrentPage = true;
                 let data = mapState(this.mapState, this.store.getState(), this.data);
-                if(data != null) this.setData(data);
-                if(onShow) onShow.apply(this, args);
+                if (data != null) this.setData(data);
+                if (onShow) onShow.apply(this, args);
             }
-            vm.onHide = function(...args) {
+            vm.onHide = function (...args) {
                 this.$isCurrentPage = false;
-                if(onHide) onHide.apply(this, args);
+                if (onHide) onHide.apply(this, args);
             }
             vm.onUnload = unmountRedux(onUnload);
-        } else if(type === 'Component') {
-            let {created, attached, detached} = vm;
-            vm.created = function(...args) {
-                if(this.app == null) {
+        } else if (type === 'Component') {
+            let {
+                created,
+                attached,
+                detached
+            } = vm;
+            vm.created = function (...args) {
+                if (this.app == null) {
                     console.warn('组件未注入app实例，无法使用Redux')
                 } else {
                     this.store = this.app.store;
-                    if(created) created.apply(this, args);
+                    if (created) created.apply(this, args);
                 }
             }
+            vm.$isCurrentPage = true;
             vm.attached = mountRedux(attached);
             vm.detached = unmountRedux(detached);
+        } else {
+            throw new Error('不合法的wxa组件类型');
         }
     };
 }
---
+++ packages/wxa-redux/test/wxa-redux.test.js
@@ -0,0 +1,244 @@
+import {wxaRedux, combineReducers} from '../src/index'
+
+let todo = function(state=[], action) {
+    switch(action.type) {
+        case 'add' : {
+            state = [...state, action.payload]
+            return state;
+        }
+        default : return state;
+    }
+}
+
+let todoDel = function(state=[], action) {
+    switch(action.type) {
+        case 'del' : {
+            state = [...state, action.payload]
+            return state;
+        }
+        default : return state;
+    }
+}
+
+let setData = function(data) {
+    this.data = {
+        ...this.data,
+        ...data
+    }
+}
+
+test('mount redux store and middlewares', ()=>{
+    let reduxFn = wxaRedux({
+        reducers: combineReducers({todo})
+    }, 'App');
+
+    let mw = jest.fn();
+    let reduxFnMW = wxaRedux({
+        reducers: combineReducers({todo}),
+        middlewares: [function(store){
+            return function(){
+                mw();
+            }
+        }]
+    }, 'App');
+
+    let vm = {}, vm2 = {};
+
+    reduxFn(vm);
+
+    reduxFnMW(vm2);
+
+    expect(vm.store).not.toBeNull();
+    expect(mw.mock.calls.length).toBe(1);
+});
+
+test('throw error while type undefined', ()=>{
+    let reduxFn = wxaRedux({}, 'Hello');
+
+    let page = {};
+
+    expect(()=>{
+        reduxFn(page)
+    }).toThrowErrorMatchingSnapshot();
+});
+
+describe('Page', ()=>{
+    let reduxFnApp = wxaRedux({
+        reducers: combineReducers({
+            todo,
+            todoDel
+        })
+    }, 'App');
+    let app = {}; reduxFnApp(app);
+    
+    let reduxFnPage = wxaRedux({}, 'Page');
+
+    test('subscribe redux', ()=>{
+        let page = {
+            app,
+            setData
+        }; 
+    
+        reduxFnPage(page);
+    
+        expect(page.unsubscribe).toBeFalsy();
+        page.onLoad();
+        expect(page.unsubscribe).toBeDefined();
+    });
+
+    test('call with default function', ()=>{
+        const onLoad = jest.fn();
+        const onShow = jest.fn();
+        const onHide = jest.fn();
+        const onUnload = jest.fn();
+
+        let page = {
+            app,
+            setData,
+            onLoad, onShow, onHide, onUnload
+        };
+
+        reduxFnPage(page);
+
+        expect(onLoad.mock.calls.length).toBe(0);
+        page.onLoad({scene: 110});
+        expect(onLoad.mock.calls.length).toBe(1);
+        expect(onLoad).toHaveBeenCalledWith({scene: 110});
+        page.onLoad();
+        page.onLoad();
+        expect(onLoad.mock.calls.length).toBe(3);
+        
+        expect(onShow.mock.calls.length).toBe(0);
+        page.onShow();
+        expect(onShow.mock.calls.length).toBe(1);
+        expect(page.$isCurrentPage).toBe(true);
+        
+        expect(onHide.mock.calls.length).toBe(0);
+        page.onHide();
+        expect(onHide.mock.calls.length).toBe(1);
+        expect(page.$isCurrentPage).toBe(false);
+        
+        expect(onUnload.mock.calls.length).toBe(0);
+        page.onUnload();
+        expect(onUnload.mock.calls.length).toBe(1);
+        expect(page.unsubscribe).toBeNull();
+
+    })
+
+    test('update data', ()=>{
+         let page = {
+             app,
+             mapState: {
+                 todoList: (state)=>state.todo
+             },
+             setData
+         };
+
+         reduxFnPage(page);
+
+         page.onLoad();
+
+         page.store.dispatch({type: 'add', payload: 'test+1'});
+         expect(page.data).toBeFalsy();
+         expect(app.store.getState().todo.length).toBe(1);
+         
+         page.onShow();
+         page.store.dispatch({type: 'add', payload: 'test+2'});
+         page.store.dispatch({type: 'add', payload: 'test+3'});
+         expect(page.data.todoList.length).toBe(3);
+
+
+         page.store.dispatch({type: 'del', payload: 'test+3'});
+         expect(page.data.todoList.length).toBe(3);
+         expect(app.store.getState().todo.length).toBe(3);
+         
+         page.onHide();
+         page.store.dispatch({type: 'add', payload: 'test+2'});
+         expect(page.data.todoList.length).toBe(3);
+         expect(app.store.getState().todo.length).toBe(4);
+         
+         page.onShow();
+         page.onShow();
+         expect(page.data.todoList.length).toBe(4);
+
+         page.onUnload();
+         page.onUnload();
+    })
+
+    test('throw error while not mount app', ()=>{
+        let page = {};
+
+        expect(()=>{
+            reduxFnPage(page)
+        }).toThrowErrorMatchingSnapshot();
+    });
+
+});
+
+describe('Component', ()=>{
+    let reduxFnApp = wxaRedux({
+        reducers: combineReducers({todo})
+    }, 'App');
+    let app = {}; reduxFnApp(app);
+    
+    let reduxFnComponent = wxaRedux({}, 'Component');
+
+    test('mount redux', ()=>{
+        const created = jest.fn();
+        let com = {
+            app,
+            created
+        };
+        let q = {scene: 110};
+
+        reduxFnComponent(com);
+        expect(com.store).not.toBeNull();
+        com.created(q);
+        expect(created.mock.calls.length).toBe(1);
+        expect(created).toHaveBeenCalledWith(q);
+    });
+
+    test('dispatch data to component', ()=>{
+        const attached = jest.fn();
+        const detached = jest.fn();
+
+        let com = {
+            app,
+            attached,
+            detached,
+            mapState: {
+                todoList: (state)=>state.todo
+            },
+            setData
+        }
+        
+        reduxFnComponent(com);
+        com.created();
+        
+        com.attached();
+        expect(attached.mock.calls.length).toBe(1);
+        expect(com.data).toBeFalsy();
+        expect(com.unsubscribe).not.toBeFalsy();
+
+        com.store.dispatch({type: 'add', payload: 'testcom'});
+        expect(com.data.todoList.length).toBe(1);
+        com.store.dispatch({type: 'add', payload: 'testcom'});
+        com.store.dispatch({type: 'add', payload: 'testcom'});
+        expect(com.data.todoList.length).toBe(3);
+
+        com.detached();
+        expect(com.unsubscribe).toBeNull();
+    })
+
+    test('console warn while not mount app', ()=>{
+        let com = {};
+        global.console = {
+            warn: jest.fn()
+        }
+
+        reduxFnComponent(com);
+        com.created();
+
+        expect(global.console.warn.mock.calls.length).toBe(1);
+    });
+})
