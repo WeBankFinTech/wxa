af0badb4a5ece31ba77f379854b052348e1bb85c
Genuifx <1119127128@qq.com>
2018-02-23 10:51:52 +0800

    feat: mixins support nesting copy

--- packages/wxa-core/test/mixin.test.js
+++ packages/wxa-core/test/mixin.test.js
@@ -54,6 +54,39 @@ describe('mixin', ()=>{
         expect(common.methods.test2.mock.calls.length).toBe(1);
     });
 
+    test('nesting mixins', ()=>{
+        let nest = {
+            methods: {
+                test3: jest.fn(),
+            },
+        };
+        let common = {
+            mixins: [nest],
+            data: {
+                a: 1,
+                c: 3,
+            },
+            methods: {
+                test: jest.fn(),
+                test2: jest.fn(),
+            },
+        };
+        let vm = {
+            mixins: [common],
+            data: {
+                a: 2,
+                b: 2,
+            },
+            methods: {
+                test: jest.fn(),
+            },
+        };
+
+        let dvm = mixin(vm);
+        dvm.methods.test3();
+        expect(dvm.methods.test3).toHaveBeenCalled();
+    });
+
     test('merge hook', ()=>{
         let common = {
             onLoad: jest.fn(),
