78487963d8f7492ad6180b7561775c6ec139c6db
Genuifx <1119127128@qq.com>
2018-03-11 23:28:22 +0800

    feat: parse component's path

--- packages/wxa-cli/src/compile-config.js
+++ packages/wxa-cli/src/compile-config.js
@@ -1,7 +1,9 @@
-import {getDistPath, writeFile, readFile} from './utils';
+import {getDistPath, writeFile, readFile, error, isFile, warn} from './utils';
 import {info} from './utils';
 import path from 'path';
 import {AsyncSeriesHook} from 'tapable';
+import PathParser from './helpers/pathParser';
+import findRoot from 'find-root';
 
 class CConfig {
     constructor(src, dist) {
@@ -10,16 +12,70 @@ class CConfig {
         this.dist = dist || 'dist';
         this.type = 'json';
         this.code = '';
+        this.modulesPath = path.join(this.current, 'node_modules', path.sep);
+        this.localVisualPath = path.join(this.current, 'local', path.sep);
+        this.npmPath = path.join(this.current, dist, 'npm', path.sep);
+        this.localPath = path.join(this.current, dist, 'local', path.sep);
         this.hooks = {
             optimizeAssets: new AsyncSeriesHook(['code', 'compilation']),
         };
     }
+
+    copyComponents(com, isNpm=true) {
+        let extOfCom = ['.wxml', '.wxss', '.js', '.json'];
+
+        extOfCom.forEach((ext)=>{
+            let uri = isNpm ? path.join(this.modulesPath, com, ext) : com+path.sep+ext;
+
+            if (isFile(uri)) {
+                let target;
+                if (isNpm) {
+                    target = path.join(this.npmPath, com);
+                } else {
+                    target = path.join(this.localPath, path.parse(com).base);
+                }
+                info(`write ${isNpm ? 'npm' : 'local'} com`, path.relative(this.current, target));
+                writeFile(target, readFile(uri));
+            } else if (ext === '.json') {
+                warn(com+'组件不存在json配置文件');
+            }
+        });
+    }
+
+    resolveComponents(code, opath) {
+        if (!code.usingComponents) return code;
+
+        let coms = code.usingComponents;
+        Object.keys(coms).forEach((key)=>{
+            let com = code.usingComponents[key];
+            let pret = new PathParser().parse(com);
+            if (pret.isNodeModule || pret.isAbsolute) {
+                // copy npm or local components, generate new path;
+                this.copyComponents(com, pret.isNodeModule);
+                let resolved = path.join(path.relative(opath.dir, pret.isNodeModule ? this.modulesPath : this.localVisualPath), com);
+                coms[key] = resolved;
+            }
+        });
+        code.usingComponents = coms;
+        return code;
+    }
+
     compile(content, opath) {
         if (content == null) {
             content = readFile(path.join(opath.dir, opath.base));
             if (content == null) throw new Error('打开文件失败 ', path.join(opath.dir, opath.base));
         }
-        this.code = content;
+        try {
+            content = JSON.parse(content);
+            // 编译组件
+            content = this.resolveComponents(content, opath);
+        } catch (e) {
+            error('config有误, 请检查格式');
+            error(e);
+            return Promise.reject(e);
+        }
+
+        this.code = JSON.stringify(content, void(0), 4);
         return this.hooks.optimizeAssets.promise(content, this).then((err)=>{
             if (err) return Promise.reject(err);
             let target = getDistPath(opath, 'json', this.src, this.dist);
