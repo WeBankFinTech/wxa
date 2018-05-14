52e6cbcddc71c69b901c0e4e5d7dc6d5db39b360
Genuifx <1119127128@qq.com>
2018-05-14 15:04:06 +0800

    feat(wxa.js, message, toast): export some lodash function, add message and toast function for using.
    nowaday, wxa will export `debounce`, `throttle`, `delay`, `once`function for WMP. new feature
    `message` and `toast` is ready to use for developer.

---
+++ packages/wxa-core/src/utils/message.js
@@ -0,0 +1,38 @@
+import wxapi from './wxapi';
+
+class Message {
+    constructor(wxapi) {
+        this.wxapi = wxapi;
+        this.config = {
+            title: '',
+            showCancel: false,
+            confirmColor: '#09bb07',
+        };
+    }
+
+    show(title, content='', btn={}) {
+        return this.wxapi.showModal({
+            ...this.config,
+            title,
+            content,
+            ...btn,
+        });
+    }
+
+    error(title, content, btn) {
+        return this.show(title, content, btn);
+    }
+}
+
+const _message = new Message(wxapi(wx));
+const message = (title, content, opt)=>{
+    return _message.show(title, content, opt);
+};
+Object.getOwnPropertyNames(Message.prototype).forEach((key)=>{
+    message[key] = _message[key].bind(_message);
+});
+
+export {
+    message as default,
+    Message,
+};
