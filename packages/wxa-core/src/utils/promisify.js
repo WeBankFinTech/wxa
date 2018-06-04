b99ae6525126dcba10514b7ff29133304f497f54
iveswen <iveswen@webank.com>
2018-06-04 17:30:29 +0800

    chores: del

--- packages/wxa-core/src/utils/promisify.js
+++
@@ -1,7 +0,0 @@
-export default (api)=>{
-    return (options, ...params) => {
-        return new Promise((resolve, reject) => {
-            api(Object.assign({}, options, {success: resolve, fail: reject}), ...params);
-        });
-    };
-};
--- packages/wxa-ui/README.md
+++
@@ -1,5 +0,0 @@
-# wxa-ui
-ui components for wechat miniprogram development
-
-# minicode
-![minicode](./minicode.jpg)
