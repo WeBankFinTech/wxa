b8fd42a49d3398f572ffe41157d383a77fc363a5
Genuifx <1119127128@qq.com>
2018-06-04 17:30:29 +0800

    chores: del

--- packages/wxa-core/prebuild.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd node_modules/lodash
-
-sed -i '.bak' 's/module.exports = freeGlobal;/module.exports={Array:Array,Date:Date,Error:Error,Function:Function,Math:Math,Object:Object,RegExp:RegExp,String:String,TypeError:TypeError,setTimeout:setTimeout,clearTimeout:clearTimeout,setInterval:setInterval,clearInterval:clearInterval};/' _freeGlobal.js
--- packages/wxa-plugin-uglifyjs/README.md
+++
@@ -1,9 +0,0 @@
-# wxa-plugin-uglifyjs
-use uglifyjs to compress, beautify code for production
-
-# Usage
-```javascript
-    new UglifyjsPlugin({
-      ...uglifyjs configurations
-    })
-```
