e48ed190836a338524adb48f1bc4635e3d1ec671
Genuifx <1119127128@qq.com>
2018-11-01 17:43:24 +0800

    feat(copyPlugin): use copyPlugin for static assets copy

---
+++ packages/wxa-plugin-copy/index.js
@@ -0,0 +1,47 @@
+var ncp = require('ncp').ncp;
+var path = require('path');
+
+module.exports = class CopyPlugin {
+    constructor(options={}) {
+        // from, to, ignore
+        this.options = {
+            to: 'static',
+            ...options
+        }
+
+        this.options.ignore = typeof this.options.ignore === 'string' ? [this.options.ignore] : 
+        this.options.ignore ? this.options.ignore : [];
+    }   
+    apply(compiler) {
+        if(compiler.hooks == null || compiler.hooks.done == null) return;
+
+        compiler.hooks.done.tapAsync('CopyPlugin', (compilation, next)=>{
+            if(this.options.from == null) {
+                console.warn('CopyPlugin配置错误')
+                next();
+            }
+
+            this.run(compilation, next);
+        })
+    }
+    run(compilation, next) {
+        var source = path.isAbsolute(this.options.from) ? this.options.from : path.join(compilation.meta.current, this.options.from);
+        var dist = path.join(compilation.wxaConfigs.output.path, this.options.to);
+        var options = this.options;
+
+        ncp(source, dist, {
+            filter(filename){
+                return !options.ignore.some((value)=>{
+                    let reg = value instanceof RegExp ? value : new RegExp(value);
+                    return reg.test(filename);
+                });
+            }
+        }, function(err){
+            if(err) {
+                next(err)
+            }
+
+            next();
+        })
+    }
+}
