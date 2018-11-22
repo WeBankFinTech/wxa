2664a60191850bcdfe2b7fe139008442c4e73315
Genuifx <1119127128@qq.com>
2018-11-22 17:35:51 +0800

    fix(cli.create): use shelljs for helping fix window command.

--- packages/wxa-cli/src/creator.js
+++ packages/wxa-cli/src/creator.js
@@ -2,6 +2,8 @@ import {exec} from 'child_process';
 import logger from './helpers/logger';
 import fs from 'fs';
 import path from 'path';
+import shell from 'shelljs';
+
 class Creator {
     constructor(cmd) {
         this.cmdOptions = cmd;
@@ -20,17 +22,22 @@ class Creator {
                 logger.error(err);
             } else {
                 logger.info('clone', `成功下载模板 ${full}`);
-                exec(`cp -rf ./${name}/${template} ./${template} && rm -rf ${name} && mv ${template} ${name}`, function(err) {
-                    if (err) {
-                        logger.error(err);
-                    }
+                try {
+                    let cwd = process.cwd();
+                    let tmpDir = Date.now()+`_${template}`;
+
+                    shell.cp('-Rf', path.join(cwd, `./${name}/${template}/`), path.join(cwd, tmpDir));
+                    shell.rm('-rf', path.join(cwd, `./${name}/`));
+                    shell.mv(path.join(cwd, tmpDir), path.join(cwd, `${name}/`));
 
                     let filepath = path.join(process.cwd(), `${name}/package.json`);
                     let pkg = require(filepath);
                     pkg.name = name;
                     fs.writeFileSync(filepath, JSON.stringify(pkg, null, 4));
                     logger.info('success', '新建成功，请注意安装依赖');
-                });
+                } catch (e) {
+                    logger.error(e);
+                }
             }
         });
     }
