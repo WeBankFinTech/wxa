6c7f7a6ed72a82efa6664e1b584050dbeceb0d0c
hanzooo <han.ker@qq.com>
2018-12-12 17:43:15 +0800

    feat(DAP): tooltip，update stats

--- packages/wxa-plugin-dependencies-analysis/client/index.jsx
+++ packages/wxa-plugin-dependencies-analysis/client/index.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TreeMap from './components/TreeMap';
+import App from './App';
 
 ReactDOM.render(
-    <TreeMap/>, 
-    document.getElementById('root')
+  <App/>,
+  document.getElementById('root')
 );
-
-try {
-    let ws = new WebSocket(`ws://${location.host}`);
-    ws.addEventListener('message', event => {
-        console.log('ws message event: ', event);
-       
-    });
-} catch(err) {
-    console.error(err);
-}
--- packages/wxa-plugin-dependencies-analysis/src/index.js
+++ packages/wxa-plugin-dependencies-analysis/src/index.js
@@ -1,37 +1,30 @@
 require("@babel/polyfill");
-import { Stats } from './Stats'; 
+import { Stats } from './Stats';
 import { start } from './server';
-const fs = require('fs');
 
 export class DependenciesAnalysisPlugin {
-    constructor(options = {}) {
-         
+  constructor(options = {}) {
+    this.DAPserver = null;
+    this.options = options;
+  }
+
+  apply(compiler) {
+    if (!compiler.hooks || !compiler.hooks.done) return;
+
+    compiler.hooks.done.tapAsync('DependenciesAnalysisPlugin', (compilation, next) => {
+      const stats = new Stats();
+      const statsJson = stats.toStatsJson(compilation.$indexOfModule)
+
+      this.run(statsJson);
+
+    })
+  }
+
+  run(stats) {
+    if (this.DAPserver) {
+      this.DAPserver.updateData(stats);
+    } else {
+      this.DAPserver = start(stats, this.options);
     }
-
-    apply(compiler) {
-        if(!compiler.hooks || !compiler.hooks.done) return;
-
-        compiler.hooks.done.tapAsync('DependenciesAnalysisPlugin', (compilation, next)=>{
-            const stats = new Stats();
-            const statsJson = stats.toStatsJson(compilation.$indexOfModule)
-
-            fs.writeFileSync('./test-stats.json', JSON.stringify(statsJson, null, 2), 'utf-8');
-
-            start();
-            //this.run();
-        })
-    }
-
-    run(stats) {
-        if (this.DAPserver) {
-
-        } else {
-            this.DAPserver = start(stats, {
-                // host: this.opts.analyzerHost,
-                // port: this.opts.analyzerPort,
-            })
-        }
-    }
-
-}    
-
+  }
+}
