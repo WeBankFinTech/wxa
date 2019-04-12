39f05cddf01ce69e4eb40adbe76f452114c54f6e
Genuifx <genuifx@gmail.com>
2019-04-12 18:13:19 +0800

    build(cli): ignore polyfill build

--- packages/wxa-cli/scripts/buildLib.sh
+++ packages/wxa-cli/scripts/buildLib.sh
@@ -1,1 +1,1 @@
-npx webpack  ./lib/shim/promise.finally.js --mode production --env.platform=node  -o ./lib-dist/es/promise.finally.js
+# npx webpack  "./lib/shim/promise.finally.js" --mode production --env.platform=node  -o ./lib-dist/es/promise.finally.js
