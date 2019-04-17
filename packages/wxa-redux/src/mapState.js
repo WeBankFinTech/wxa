59cfa5d4f2f4c727d6749da42ebc4546b1a19903
Genuifx <genuifx@gmail.com>
2019-04-17 17:28:41 +0800

    feat(redux): bind mapState to context

--- packages/wxa-redux/src/mapState.js
+++ packages/wxa-redux/src/mapState.js
@@ -1,15 +1,20 @@
 import shallowequal from 'shallowequal'
 
-export default function mapState(map, state, source={}) {
+export default function mapState(map, state, source={}, context) {
     if(map == null) return null;
+
     let {newState, oldState} = Object.keys(map).reduce((ret, key)=>{
         if(typeof map[key] !== 'function') {
             console.log(`mapState中的${key}必须为函数`);
             return ret;
         }
+
         try {
-            ret.newState[key] = map[key](state);
-            ret.oldState[key] = map[key](source);
+            let fn = map[key];
+            if (context) fn = fn.bind(context);
+            
+            ret.newState[key] = fn(state);
+            ret.oldState[key] = fn(source);
         } catch(e) {
             throw e;
         }
