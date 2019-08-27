75dbf417f09f007d30b1002ee11b2cc393562ba6
Genuifx <genuifx@gmail.com>
2019-08-28 01:06:41 +0800

    feat(cli): inject test component
    inject test component to every page, rename directory name

--- packages/wxa-cli/scripts/buildLib.sh
+++ packages/wxa-cli/scripts/buildLib.sh
@@ -1,1 +1,3 @@
 # npx webpack  "./lib/shim/promise.finally.js" --mode production --env.platform=node  -o ./lib-dist/es/promise.finally.js
+
+cp -R ./src/tester/wxa-e2eTest ./lib-dist/wxa-e2eTest
--- packages/wxa-cli/src/tester/wxa-e2eTest/e2eRecord2js.js
+++ packages/wxa-cli/src/tester/wxa-e2eTest/e2eRecord2js.js
@@ -3,7 +3,7 @@ const ejs = require('ejs');
 
 let tpl = fs.readFileSync(__dirname + '/e2eRecord2jsTpl.ejs', 'utf8');
 // 测试代码
-let record = [{"page":"pages/index","event":"tap","id":"Pagesindex73315fe4331859e82832f81a3cd615e9","timeStamp":1566472440497,"detail":{"x":134.85546875,"y":365.14453125}},{"page":"pages/visitor/inviteDetail","event":"change","id":"Pagesvisitor_inviteDetailF43f4435c899b6657003319cf5a48988","timeStamp":1566472454334,"detail":{"value":"2019-08-22"}},{"page":"pages/visitor/inviteDetail","event":"tap","id":"Pagesvisitor_inviteDetail5ba81b4e5ef42c45b6b3ee55f8174fbc","timeStamp":1566472456176,"detail":{"x":259.03515625,"y":159.375}}];
+let record = [{'page': 'pages/index', 'event': 'tap', 'id': 'Pagesindex73315fe4331859e82832f81a3cd615e9', 'timeStamp': 1566472440497, 'detail': {'x': 134.85546875, 'y': 365.14453125}}, {'page': 'pages/visitor/inviteDetail', 'event': 'change', 'id': 'Pagesvisitor_inviteDetailF43f4435c899b6657003319cf5a48988', 'timeStamp': 1566472454334, 'detail': {'value': '2019-08-22'}}, {'page': 'pages/visitor/inviteDetail', 'event': 'tap', 'id': 'Pagesvisitor_inviteDetail5ba81b4e5ef42c45b6b3ee55f8174fbc', 'timeStamp': 1566472456176, 'detail': {'x': 259.03515625, 'y': 159.375}}];
 // ejs.renderFile(__dirname + '/e2eRecord2jsTpl.ejs', {
 //     record
 // }, (err, str) => {
@@ -15,12 +15,12 @@ let record = [{"page":"pages/index","event":"tap","id":"Pagesindex73315fe4331859
 e2eRecord2js(record, './automator.js');
 function e2eRecord2js(record, path) {
     ejs.renderFile(__dirname + '/e2eRecord2jsTpl.ejs', {
-        record
+        record,
     }, (err, str) => {
         console.log(err);
         console.log(str);
         fs.writeFileSync(path, str);
-    })
+    });
 }
 
 module.exports = e2eRecord2js;
