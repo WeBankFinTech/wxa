662c944412ccf822fb2b8911dfd1ae607c82176a
Genuifx <genuifx@gmail.com>
2020-03-04 00:33:29 +0800

    refactor(cli): refactory cli entry, move configs manager logic to getConfigs, move builder manager t

--- packages/wxa-cli/src/toolcli.js
+++ packages/wxa-cli/src/toolcli.js
@@ -94,3 +94,58 @@ class Toolcli {
 }
 
 export default Toolcli;
+
+export function spawnDevToolCli(configs, cmdOptions) {
+    let newCli = wrapWxaConfigs((subWxaConfigs, cmd)=>{
+        let cli = new Toolcli(subWxaConfigs);
+        cli.run(cmd);
+    });
+
+    let question = async ()=>await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'version',
+            message: '小程序版本号',
+            default: require(path.join(process.cwd(), 'package.json')).version || '1.0.0',
+        },
+        {
+            type: 'input',
+            name: 'desc',
+            message: '版本描述',
+            default: '版本描述',
+        },
+    ]);
+
+    if (
+        cmd.multi &&
+        wxaConfigs.thirdParty &&
+        wxaConfigs.thirdParty.length &&
+        cmd.action === 'upload'
+    ) {
+        let options = await question();
+        cmd.options = options;
+        // third party development
+        if (cmd.project) {
+            cmd.project.split(',').forEach((project)=>{
+                // specify project to compile
+                project = wxaConfigs.thirdParty.find((instance)=>instance.name===project);
+
+                if (!project) {
+                    logger.error('找不到指定的项目，请检查wxa.config.js中的三方配置');
+                    process.exit(0);
+                } else {
+                    newCli(wxaConfigs, project, cmd);
+                }
+            });
+        } else {
+            // compile and watch all projects.
+            wxaConfigs.thirdParty.forEach((project)=>{
+                newCli(wxaConfigs, project, {...cmd});
+            });
+        }
+    } else {
+        if (cmd.action === 'upload') cmd.options = await question();
+        // normal build.
+        newCli(wxaConfigs, void(0), cmd);
+    }
+}
