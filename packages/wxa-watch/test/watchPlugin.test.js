221f38ee1139a801f4f01eca41b93c65cdd030f7
Genuifx <genuifx@gmail.com>
2020-09-15 21:02:30 +0800

    feat(watch): Add components watch and computed support

--- packages/wxa-watch/test/watchPlugin.test.js
+++ packages/wxa-watch/test/watchPlugin.test.js
@@ -41,7 +41,7 @@ describe('watch plugin', ()=>{
         vm.onUnload();
     })
 
-    test('watch data', async()=>{
+    test('watch page data', async()=>{
         let mock = jest.fn();
 
         let vm = {
@@ -63,6 +63,79 @@ describe('watch plugin', ()=>{
         expect(mock).not.toBeCalled();
         
         vm.onLoad.call(vm);
+        
+        vm.data.a = 2;
+        await new Promise((resolve)=>setTimeout(resolve));
+        expect(mock).toBeCalled();
+        
+        vm.data.b.c = 3;
+        await new Promise((resolve)=>setTimeout(resolve));
+        expect(mock).toBeCalledTimes(2);
+
+        vm.data.b.d = 'x';
+        await new Promise((resolve)=>setTimeout(resolve));
+        expect(mock).toBeCalledTimes(2);
+        
+        vm.data.d[0] = 0;
+        await new Promise((resolve)=>setTimeout(resolve));
+        expect(mock).toBeCalledTimes(2);
+        
+        vm.data.d = vm.data.d.splice(0, 1, 0);
+        await new Promise((resolve)=>setTimeout(resolve));
+        expect(mock).toBeCalledTimes(3);
+    })
+
+    test('computed page data', async () => {
+        let mock = jest.fn();
+
+        let vm = {
+            data: {
+                a: 1,
+                b: {c: 2},
+                d: [1]
+            },
+            computed: {
+                computedA() {
+                    console.log('------computed')
+                    return this.data.a;
+                }
+            }
+        }
+
+        vm.setData = function(data) {
+            this.data = {...data, ...this.data};
+            mock(data);
+        }
+        watchFn(vm, 'Page');
+        
+        vm.onLoad.call(vm);
+
+        expect(vm.data.computedA).toBe(1);
+        expect(vm.$$WXAWatcher.length).toBe(1);
+    })
+
+    test('watch component data', async () => {
+        let mock = jest.fn();
+
+        let vm = {
+            data: {
+                a: 1,
+                b: {c: 2},
+                d: [1]
+            },
+            watch : {
+                a: mock,
+                'b.c': mock,
+                'd[0]': mock,
+                'b.d.c': mock
+            },
+        }
+
+        // test started.
+        watchFn(vm, 'Component');
+        expect(mock).not.toBeCalled();
+        
+        vm.lifetimes.attached.call(vm);
         
         vm.data.a = 2;
         await new Promise((resolve)=>setTimeout(resolve));
