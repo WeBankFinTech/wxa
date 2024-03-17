ad70d29fdba50876641a0a9ad48d418f78b4ba7e
Genuifx <genuifx@gmail.com>
2024-03-17 23:35:10 +0800

    refactor: deps update

--- packages/wxa-cli/src/optimizer/pxtorpx.js
+++ packages/wxa-cli/src/optimizer/pxtorpx.js
@@ -2,13 +2,13 @@ import postcss from 'postcss';
 import pxtorpx from 'postcss-pxtorpx-pro';
 import {isEmpty} from '../utils';
 
-export default function transformPixelsToRpx(mdl, wxaConfigs) {
+export default async function transformPixelsToRpx(mdl, wxaConfigs) {
     let options = wxaConfigs.optimization.transformPxToRpx;
     options = (typeof options === 'boolean' || isEmpty(options)) ? {} : options;
 
-    let css = postcss().use(pxtorpx(options)).process(mdl.code, {from: mdl.src}).css;
+    let result = await postcss([pxtorpx(options)]).process(mdl.code, {from: mdl.src});
 
-    mdl.code = css;
+    mdl.code = result.css;
 
     return mdl;
 }
